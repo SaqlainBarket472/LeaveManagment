ab6781193c6eb2be9ac7deab623559c8a59c652b
saqlain <sheikh.saqlian@gmail.com>
2026-05-18 11:41:02 +0500

    Migrations

---
+++ LeaveManagement.Infrastructure/Migrations/20260518062851_addednewcolumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addednewcolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RejectionComment",
+                table: "LeaveRequests",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RejectionComment",
+                table: "LeaveRequests");
+        }
+    }
+}
